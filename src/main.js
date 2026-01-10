7920c3f779771661d58319963f960cadd9dd8c54
dukegooner80050-netizen <dukegooner80050@gmail.com>
2026-01-11 01:30:54 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Nav from './components/Nav.vue'
+
 createApp(App).mount('#app')
-createApp(Nav).mount('#nav')
